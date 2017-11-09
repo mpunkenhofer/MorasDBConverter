22333cf391c16b081d92ee57cfce8646841bfe41
Mathias Punkenhofer <mpunkenhofer@student.tugraz.at>
2017-11-09 20:17:54 +0100

    added item id ignore support

--- MorasDBConverter.py
+++ MorasDBConverter.py
@@ -53,7 +53,11 @@ def progress_bar(value, endvalue, bar_length=20):
     sys.stdout.flush()
 
 
-def convert(db, json, converter):
+def read_item_ids(filename):
+    return [59, 74]
+
+
+def convert(db, json, converter, ignore):
     c = db.cursor()
     items = json["items"]
 
@@ -61,61 +65,68 @@ def convert(db, json, converter):
 
     timestamp = str(int(time.time()))
     errors = []
+    ignored_items = []
+
     converted_items = 0
+    db_item_id = 1
 
     for i, item in enumerate(items):
         try:
-            db_item = (   # id
-                          i + 1,
-                          # name
-                          converter.name(item),
-                          # orig name (never used)
-                          '',
-                          # origin
-                          converter.origin(item),
-                          # description.
-                          converter.description(item),
-                          # url
-                          'https://github.com/mpunkenhofer/MorasDBConverter',
-                          # patch (do not use - causes moras to crash)
-                          '',
-                          # provider
-                          'Neca',
-                          # class restrictions
-                          converter.class_restriction(item),
-                          # effects / stats
-                          converter.bonuses(item),
-                          # realm
-                          converter.realm(item),
-                          # position
-                          converter.slot(item),
-                          # type
-                          converter.type(item),
-                          # level
-                          converter.level(item),
-                          # quality
-                          converter.quality(item),
-                          # bonus
-                          converter.bonus(item),
-                          # class
-                          converter.item_class(item),
-                          # subclass
-                          converter.subclass(item),
-                          # material
-                          converter.material(item),
-                          # armor factor
-                          converter.armor_factor(item),
-                          # dps
-                          converter.dps(item),
-                          # speed
-                          converter.speed(item),
-                          # max level
-                          converter.max_level(item),
-                          # damage type
-                          converter.damage_type(item),
-                          # last update
-                          timestamp
-                      )
+            if not ignore or converter.id(item) not in ignore:
+                db_item = (   # id
+                              db_item_id,
+                              # name
+                              converter.name(item),
+                              # orig name (never used)
+                              '',
+                              # origin
+                              converter.origin(item),
+                              # description.
+                              converter.description(item),
+                              # url
+                              'https://github.com/mpunkenhofer/MorasDBConverter',
+                              # patch (do not use - causes moras to crash)
+                              '',
+                              # provider
+                              'Neca',
+                              # class restrictions
+                              converter.class_restriction(item),
+                              # effects / stats
+                              converter.bonuses(item),
+                              # realm
+                              converter.realm(item),
+                              # position
+                              converter.slot(item),
+                              # type
+                              converter.type(item),
+                              # level
+                              converter.level(item),
+                              # quality
+                              converter.quality(item),
+                              # bonus
+                              converter.bonus(item),
+                              # class
+                              converter.item_class(item),
+                              # subclass
+                              converter.subclass(item),
+                              # material
+                              converter.material(item),
+                              # armor factor
+                              converter.armor_factor(item),
+                              # dps
+                              converter.dps(item),
+                              # speed
+                              converter.speed(item),
+                              # max level
+                              converter.max_level(item),
+                              # damage type
+                              converter.damage_type(item),
+                              # last update
+                              timestamp
+                          )
+            else:
+                ignored_items.append(converter.identifier(item))
+                continue
 
         except NotImplementedError as e:
             errors.append('Not Implemented Error: ' + str(e))
@@ -127,6 +138,7 @@ def convert(db, json, converter):
         else:
             c.execute("Insert INTO items VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", db_item)
             converted_items += 1
+            db_item_id += 1
 
         # only update progress bar once every 100 items
         if i % 100 == 0:
@@ -135,8 +147,14 @@ def convert(db, json, converter):
     # done
     progress_bar(len(items), len(items))
 
+    if ignored_items:
+        print('\n\nIgnored items: %d' % len(ignored_items))
+        for i in ignored_items:
+            print('Ignored: ' + i)
+        print()
+
     if errors_enabled:
-        print('\nError Report: %d errors\n' % len(errors))
+        print('\n\nError Report: %d errors' % len(errors))
         for e in errors:
             print(e)
         print()
@@ -147,12 +165,18 @@ def convert(db, json, converter):
 def main():
     parser = argparse.ArgumentParser(description='Converts a .json daoc db to moras db format.')
 
-    parser.add_argument('database', type=str, help='daoc item database file (.json)')
-    parser.add_argument('metadata', type=str, help='daoc item database metadata file (.json)')
-    parser.add_argument('-o', '--output', type=str, default='items.db3', help='output file name (moras database)')
-    parser.add_argument('-e', '--errors', help='display conversion errors', action='store_true')
-    parser.add_argument('-i', '--ignore', metavar='ID', nargs='+', type=int, help='ignore item(s) with a matching ID')
-    parser.add_argument('-if', '--ignore-file', type=str, help='same as -i but input is a file')
+    parser.add_argument('database', type=str,
+                        help='daoc item database file (.json)')
+    parser.add_argument('metadata', type=str,
+                        help='daoc item database metadata file (.json)')
+    parser.add_argument('-o', '--output', type=str, default='items.db3',
+                        help='output file name (moras database)')
+    parser.add_argument('-e', '--errors', action='store_true',
+                        help='display conversion errors')
+    parser.add_argument('-i', '--ignore', metavar='ID', nargs='+', type=int,
+                        help='ignore item(s) with a matching ID')
+    parser.add_argument('-if', '--ignore_file', type=str,
+                        help='same as -i but parameter is a file which contains the IDs')
 
     args = parser.parse_args()
 
@@ -167,9 +191,16 @@ def main():
         print("Error: metadata file '%s' does not exists." % args.metadata)
         return 1
 
+    if args.ignore_file and not file_exists(args.ignore_file):
+        print("Error: ignore file '%s' does not exists." % args.ignore_file)
+        return 1
+
     if file_exists(args.output):
         print("Error: output file '%s' already exists." % args.output)
         return 1
+
+    ignored_item_ids = [] if not args.ignore else args.ignore
+    ignored_item_ids += [] if not args.ignore_file else read_item_ids(args.ignore_file)
 
     print("Creating database '%s' ..." % args.output)
     connection = sqlite3.connect(args.output)
@@ -180,7 +211,7 @@ def main():
         data = json.load(json_file)
         print("Done loading '%s' ..." % args.database)
 
-        convert(connection, data, MorasConverter(args.metadata))
+        convert(connection, data, MorasConverter(args.metadata), ignored_item_ids)
 
     # Close
     connection.commit()
